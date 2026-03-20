80cfc28c2c75c0d1def2808a8dd8c8dc0269e0b2
juanlarrigandiere-cmd <juan.larrigandiere@fedea.com.ar>
2026-03-20 08:44:58 -0300

    Add files via upload
    Primera version

---
+++ sw (1).js
@@ -0,0 +1,36 @@
+const CACHE='ls-club-v1';
+const ASSETS=['/','index.html'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    )).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return res;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('/')))
+  );
+});
+
+self.addEventListener('push',e=>{
+  const data=e.data?e.data.json():{};
+  self.registration.showNotification(data.title||'Latitude Shine',{
+    body:data.body||'Tenés novedades en tu cuenta',
+    icon:'/icon-192.png',
+    badge:'/icon-192.png'
+  });
+});
