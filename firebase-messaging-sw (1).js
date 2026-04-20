145ccc85de9ca1531894ecd2347f8dcc066e1e61
juanlarrigandiere-cmd <juan.larrigandiere@fedea.com.ar>
2026-04-20 17:44:56 -0300

    Add files via upload

---
+++ firebase-messaging-sw (1).js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDe-tvKa4ydct2XQMX5EeRiYBvlE0OsDNk",
+  authDomain: "latitude-shine.firebaseapp.com",
+  projectId: "latitude-shine",
+  storageBucket: "latitude-shine.firebasestorage.app",
+  messagingSenderId: "216154563039",
+  appId: "1:216154563039:web:ab13cf27739b7213a7a46a"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  const { title, body, icon } = payload.notification || {};
+  self.registration.showNotification(title || 'Latitude Shine', {
+    body: body || '',
+    icon: icon || 'https://juanlarrigandiere-cmd.github.io/Latitudeshine/icon-192.png',
+    badge: 'https://juanlarrigandiere-cmd.github.io/Latitudeshine/icon-192.png',
+    data: payload.data || {}
+  });
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('https://juanlarrigandiere-cmd.github.io/Latitudeshine/'));
+});
